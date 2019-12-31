1428cd86e37da51d73eb149d9965ce7b00fc82d5
etain <etainclub@gmail.com>
2020-01-01 03:09:59 +0900

    v1.1.0

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset', 'module:react-native-dotenv'],
+  env: {
+    "production": 
+		  { "plugins": ["transform-remove-console"] } 
+  }  
 };
